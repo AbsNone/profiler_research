7481be0d52da74f0c4bf2d65822907a573f38de6
Eugene <rejgan318@gmail.com>
2023-07-07 21:14:17 +0300

    getpixel v.1

---
+++ profiler_research_01.py
@@ -0,0 +1,19 @@
+"""
+# Profiler Research v 1.0
+"""
+from PIL import Image
+
+
+def total_colors_v_1_0(img: Image) -> int:
+    total_color = set()
+    for x in range(img.width):
+        for y in range(img.height):
+            total_color.add(img.getpixel((x, y)))
+
+    return len(total_color)
+
+
+if __name__ == '__main__':
+    image_name = "data/get_background_color.png"
+    image = Image.open(image_name)
+    print(f"{total_colors_v_1_0(image)=} цветов")
